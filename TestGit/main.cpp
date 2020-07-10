af37eddfb9655bc3478e3148e2d860cd03b1852a
maxvio <viorel.maxim@gmail.com>
2020-07-10 14:37:46 -0400

    Attempt to create git

---
+++ TestGit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestGit
+//
+//  Created by viorel maxim on 2020-07-10.
+//  Copyright © 2020 viorel maxim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
