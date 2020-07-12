1b936622f5409bca32c2fdecce1a931c70ce3e98
maxvio <viorel.maxim@gmail.com>
2020-07-12 10:00:04 -0400

    Initial Commit

---
+++ Array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Array
+//
+//  Created by viorel maxim on 2020-07-12.
+//  Copyright © 2020 viorel maxim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
